0f760c3b178fc3a0b9438bb119009942ff6e0d0e
mquat <meeryuyu@gmail.com>
2022-07-09 00:35:56 +0900

    #5 photo list added

--- PhotoApp/photo/views.py
+++ PhotoApp/photo/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Photo
 
-# Create your views here.
 def photo_list(request):
-    return render(request, 'photo/photo_list.html', {})
+    photos = Photo.objects.all()
+    return render(request, 'photo/photo_list.html', {'photos':photos})
+
+#get_object_or_404: 모델로부터 데이터를 찾되, 없으면 404에러 반환 (데이터 찾는 기준: pk, 즉 id)
+def photo_detail(request,pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, 'photo/photo_detail.html', {'photo':photo})
